ab59cbe25127f523a4b034079ad7b2f9d5b2ba54
akswart <akswart@gmail.com>
2020-04-29 16:42:48 -0600

    started on proj

--- hw8/code/parallel_step_opt_graphing.py
+++ hw8/code/parallel_step_opt_graphing.py
@@ -8,9 +8,20 @@ Created on Wed Apr 22 22:06:54 2020
 Graphing the output of hw8_exC_parallel_step_opt.py given in outputfile.csv
 
 The provided output.csv is from a run done on my server with the following parameters:
-    beg_scale = .5
-    end_scale = 6
+    beg_scale = .1
+    end_scale = 5
     num_point = 500
 
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+plt.close('all')
+df = pd.read_csv("outputfile.csv")
+#plt.figure()
+df.plot(kind='scatter',x='Scale',y='Time')
+plt.xlabel("Multiplier from suggested values for grid size")
+#plt.figure()
+df.plot(kind='scatter',x='Scale',y='Iterations')
+plt.xlabel("Multiplier from suggested values for grid size")
