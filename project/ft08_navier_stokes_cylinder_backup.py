36abbb56ad394d6b4ed5d1530d9e2005d407f976
akswart <akswart@gmail.com>
2020-04-30 19:20:24 -0600

    for some reason it wont run nicely in spyder

--- project/ft08_navier_stokes_cylinder_backup.py
+++ project/ft08_navier_stokes_cylinder_backup.py
@@ -9,19 +9,16 @@ Scheme (IPCS).
 See https://fenicsproject.org/pub/tutorial/html/._ftut1009.html for theory and 
 unmodified code.
 
-Also, for some wierd reason, its wasnt writing correctly to the hdf5 and xdmf 
-outputs when I ran it in spyder, but ran perfectly from command line. No idea why.
-
 """
 
-import fenics as fs
-from fenics import dot,dx,nabla_grad,ds,inner,div # Import math operations by name to clean up code
-import mshr
-#from mshr import *
+from fenics import *
+from mshr import *
+
 import numpy as np
+
+
 import time
 
-from tqdm import tqdm_gui
 from tqdm import tqdm # Way lower overhead than ProgressBar 60ns per iter vs 800ns see github
 
 import os
@@ -140,7 +137,7 @@ elif os.name == 'posix': # For linux and mac
             except:
                 raise UserWarning("tar not found, compressed backup not made, output dir will be deleted")
                 os.system('rm -rf navier_stokes_cylinder')
-             #   """
+         
 else: # System not recognized
     raise UserWarning("system not recognized, compressed backup not made, output dir must be deleted manually")
 
@@ -154,14 +151,14 @@ rho = 1            # density
 
 print("Creating mesh")
 # Create mesh
-channel = mshr.Rectangle(fs.Point(0, 0), fs.Point(2.2, 0.41))
-cylinder = mshr.Circle(fs.Point(0.2, 0.2), 0.05)
+channel = Rectangle(Point(0, 0), Point(2.2, 0.41))
+cylinder = Circle(Point(0.2, 0.2), 0.05)
 domain = channel - cylinder
-mesh = mshr.generate_mesh(domain, 32) # Orig val 64, try lower?
+mesh = generate_mesh(domain, 32) # Orig val 64, try lower?
 
 # Define function spaces
-V = fs.VectorFunctionSpace(mesh, 'P', 2)
-Q = fs.FunctionSpace(mesh, 'P', 1)
+V = VectorFunctionSpace(mesh, 'P', 2)
+Q = FunctionSpace(mesh, 'P', 1)
 
 
 print("Defining boundary conds")
@@ -175,40 +172,40 @@ cylinder = 'on_boundary && x[0]>0.1 && x[0]<0.3 && x[1]>0.1 && x[1]<0.3'
 inflow_profile = ('4.0*1.5*x[1]*(0.41 - x[1]) / pow(0.41, 2)', '0')
 
 # Define boundary conditions
-bcu_inflow = fs.DirichletBC(V, fs.Expression(inflow_profile, degree=2), inflow)
-bcu_walls = fs.DirichletBC(V, fs.Constant((0, 0)), walls)
-bcu_cylinder = fs.DirichletBC(V, fs.Constant((0, 0)), cylinder)
-bcp_outflow = fs.DirichletBC(Q, fs.Constant(0), outflow)
+bcu_inflow = DirichletBC(V, Expression(inflow_profile, degree=2), inflow)
+bcu_walls = DirichletBC(V, Constant((0, 0)), walls)
+bcu_cylinder = DirichletBC(V, Constant((0, 0)), cylinder)
+bcp_outflow = DirichletBC(Q, Constant(0), outflow)
 bcu = [bcu_inflow, bcu_walls, bcu_cylinder]
 bcp = [bcp_outflow]
 
 # Define trial and test functions
-u = fs.TrialFunction(V)
-v = fs.TestFunction(V)
-p = fs.TrialFunction(Q)
-q = fs.TestFunction(Q)
+u = TrialFunction(V)
+v = TestFunction(V)
+p = TrialFunction(Q)
+q = TestFunction(Q)
 
 # Define functions for solutions at previous and current time steps
-u_n = fs.Function(V)
-u_  = fs.Function(V)
-p_n = fs.Function(Q)
-p_  = fs.Function(Q)
+u_n = Function(V)
+u_  = Function(V)
+p_n = Function(Q)
+p_  = Function(Q)
 
 # Define expressions used in variational forms
 U  = 0.5*(u_n + u)
-n  = fs.FacetNormal(mesh)
-f  = fs.Constant((0, 0))
-k  = fs.Constant(dt)
-mu = fs.Constant(mu)
-rho = fs.Constant(rho)
+n  = FacetNormal(mesh)
+f  = Constant((0, 0))
+k  = Constant(dt)
+mu = Constant(mu)
+rho = Constant(rho)
 
 # Define symmetric gradient
 def epsilon(u):
-    return fs.sym(fs.nabla_grad(u))
+    return sym(nabla_grad(u))
 
 # Define stress tensor
 def sigma(u, p):
-    return 2*mu*epsilon(u) - p*fs.Identity(len(u))
+    return 2*mu*epsilon(u) - p*Identity(len(u))
 
 
 # Define variational problem for step 1
@@ -217,8 +214,8 @@ F1 = rho*dot((u - u_n) / k, v)*dx \
    + inner(sigma(U, p_n), epsilon(v))*dx \
    + dot(p_n*n, v)*ds - dot(mu*nabla_grad(U)*n, v)*ds \
    - dot(f, v)*dx
-a1 = fs.lhs(F1)
-L1 = fs.rhs(F1)
+a1 = lhs(F1)
+L1 = rhs(F1)
 
 # Define variational problem for step 2
 a2 = dot(nabla_grad(p), nabla_grad(q))*dx
@@ -229,60 +226,60 @@ a3 = dot(u, v)*dx
 L3 = dot(u_, v)*dx - k*dot(nabla_grad(p_ - p_n), v)*dx
 
 # Assemble matrices
-A1 = fs.assemble(a1)
-A2 = fs.assemble(a2)
-A3 = fs.assemble(a3)
+A1 = assemble(a1)
+A2 = assemble(a2)
+A3 = assemble(a3)
 
 # Apply boundary conditions to matrices
 [bc.apply(A1) for bc in bcu]
 [bc.apply(A2) for bc in bcp]
 
 # Create XDMF files for visualization output
-xdmffile_u = fs.XDMFFile('navier_stokes_cylinder/velocity.xdmf')
-xdmffile_p = fs.XDMFFile('navier_stokes_cylinder/pressure.xdmf')
+xdmffile_u = XDMFFile('navier_stokes_cylinder/velocity.xdmf')
+xdmffile_p = XDMFFile('navier_stokes_cylinder/pressure.xdmf')
 
-hdf = fs.HDF5File(mesh.mpi_comm(), "navier_stokes_cylinder/file.h5", "w")
-hdf.write(mesh, "/mesh")
+#hdf = HDF5File(mesh.mpi_comm(), "navier_stokes_cylinder/file.h5", "w")
+#hdf.write(mesh, "/mesh")
+File('navier_stokes_cylinder/cylinder.xml.gz') << mesh
 
 
 # Create time series (for use in reaction_system.py)
-timeseries_u = fs.TimeSeries('navier_stokes_cylinder/velocity_series')
-timeseries_p = fs.TimeSeries('navier_stokes_cylinder/pressure_series')
+timeseries_u = TimeSeries('navier_stokes_cylinder/velocity_series')
+timeseries_p = TimeSeries('navier_stokes_cylinder/pressure_series')
 
 # Save mesh to file (for use in reaction_system.py)
 #File('navier_stokes_cylinder/cylinder.xml.gz') << mesh
 
 # Create progress bar
-progress = fs.Progress('Time-stepping',num_steps)
-fs.set_log_level(fs.LogLevel.PROGRESS)
+progress = Progress('Time-stepping',num_steps)
+set_log_level(LogLevel.PROGRESS)
 
-
-# Time-stepping
 print("Time Stepping")
+# Time-stepping
 t = 0
 for n in tqdm(range(num_steps)):
 
-    fs.set_log_level(fs.LogLevel.ERROR) # Only log explody stuff
+    set_log_level(LogLevel.ERROR) # Only log explody stuff
     # Update current time
     t += dt
 
     # Step 1: Tentative velocity step
-    b1 = fs.assemble(L1)
+    b1 = assemble(L1)
     [bc.apply(b1) for bc in bcu]
-    fs.solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg')
+    solve(A1, u_.vector(), b1, 'bicgstab', 'hypre_amg')
 
     # Step 2: Pressure correction step
-    b2 = fs.assemble(L2)
+    b2 = assemble(L2)
     [bc.apply(b2) for bc in bcp]
-    fs.solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg')
+    solve(A2, p_.vector(), b2, 'bicgstab', 'hypre_amg')
 
     # Step 3: Velocity correction step
-    b3 = fs.assemble(L3)
-    fs.solve(A3, u_.vector(), b3, 'cg', 'sor')
+    b3 = assemble(L3)
+    solve(A3, u_.vector(), b3, 'cg', 'sor')
 
     # Plot solution
-    #fs.plot(u_, title='Velocity')
-    #fs.plot(p_, title='Pressure')
+    #plot(u_, title='Velocity')
+    #plot(p_, title='Pressure')
 
     # Save solution to file (XDMF/HDF5)
     xdmffile_u.write(u_, t)
